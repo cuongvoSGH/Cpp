8b047f9212de8cb494b24531593bdeba00613616
cuongvoSGH <cuongvo.polnd@gmail.com>
2024-03-11 10:18:01 +0100

    Day 2 Excesise

---
+++ cpp_day2.cpp
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int side;
+    cout << "Input the side amount: ";
+    cin >> side;
+    cout << "Volume of the cube: " << side * side * side << endl; 
+    cout << endl;
+    int w = 1, l = 1;
+    cout << "Input width and length of rectangle: ";
+    cin >> w >> l;
+    cout << "Area of the rectangle: " << w * l << endl;
+    cout << endl;
+    char s;
+    cout << "Input char: ";
+    cin >> s;
+    char lowerChar = tolower(s);
+    cout << "Lower case: " << lowerChar << endl;
+    cout << endl;
+    int a;
+    cout << "Input integer: ";
+    cin >> a;
+    cout << "You entered the number " << a << endl;
+    cout << endl;
+    string name;
+    cout << "What's your name: ";
+    cin >> name;
+    cout << "Hello " << name << "!!!!!!";
+    return 0;
+}
